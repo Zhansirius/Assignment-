33499d54f88ac4cbc71d1a50dc7382af15e16834
Zhansirius <sergeyevzhansir@gmail.com>
2026-01-30 20:18:40 +0500

    Assignment 3

--- src/Projects.java
+++
@@ -1,9 +0,0 @@
-public class Projects {
-    public String pr_name;
-    public Projects(String pr_name) {
-        this.pr_name = pr_name;
-    }
-    public void display() {
-        System.out.println("Project name: " + pr_name);
-    }
-}
